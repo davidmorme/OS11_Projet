066e6d9d460170a0a49036d1c80bdecb339e4b48
davidmorme <davidmorameza@hotmail.com>
2022-12-01 16:59:16 +0100

    Update Heuristics.py
    Added 2 new heuristics and fixed the one that was already there

--- Heuristics.py
+++ Heuristics.py
@@ -35,8 +35,9 @@ D=np.array([[np.sqrt((coordXY[i,0]-coordXY[j,0])**2+(coordXY[i,1]-coordXY[j,1])*
 #Autre manner de calculer la distance
 D=np.zeros((n+1,n+1))
 for i in range(0,n+1):
-    for j in range(0,n+1):
+    for j in range(i+1,n+1):
         D[i,j]=np.sqrt((coordXY[i,0]-coordXY[j,0])**2+(coordXY[i,1]-coordXY[j,1])**2)
+        D[j,i]=D[i,j]
 '''
 
 pd.DataFrame(ProfitC).to_csv('ProfitsClients.csv')
@@ -44,21 +45,80 @@ pd.DataFrame(D).to_csv('Distances.csv')
 
 
 #%%
-
+#### Profit ####
 order=np.argsort(-ProfitC)
 
 maxD=100
-sumaD=D[0,order[0]]
-sumaP=ProfitC[order[0]]
-i=1
-while i<n:
-    sumaD += D[order[i-1]+1,order[i]+1]
-    if sumaD > maxD: break
-    sumaP += ProfitC[order[i]]
-    i+=1
-    
-ClientsV=order[0:i]+1
+sumaD=0
+sumaP=0
+ClientsV=[]
+i=0
+k=0
+for k in range(0,n):
+    j=order[k]+1
+    if sumaD + D[i,j]+D[j,0] > maxD: continue
+    sumaD += D[i,j]
+    sumaP += ProfitC[j-1]
+    i=j
+    ClientsV.append(j)
 
+sumaD += D[i,0]
 
 #%%
 
+### Plus proche Voisin ####
+
+maxD=100
+sumaD=0
+sumaP=0
+
+
+DT=D.copy() #Distance temporel
+for j in range(0,n+1): DT[j,j]=100
+
+i=0
+ClientsV=[]
+while True:
+    pv=np.argmin(DT[i])
+    if sumaD + D[i,pv] + D[pv,0] > maxD: break
+    sumaD += D[i,pv]
+    sumaP += ProfitC[pv-1]
+    for j in range(0,n+1): 
+        DT[i,j]=100 
+        DT[j,i]=100
+    ClientsV.append(pv)
+    i=pv
+
+sumaD += D[i,0]
+#%%
+
+### Profit/D ####
+
+maxD=100
+sumaD=0
+sumaP=0
+
+
+ProfitCT=ProfitC.copy() #Profit temporel
+DT=D.copy() #Distance temporel
+for j in range(0,n+1): DT[j,j]=1000
+
+i=0
+ClientsV=[]
+while True:
+    Coef=ProfitCT/DT[i,1:]
+    order=np.argsort(-Coef)
+    
+    for k in range(0,sum(ProfitCT>0)):
+        j=order[k]+1
+        if sumaD + D[i,j] + D[j,0] <= maxD: break
+        
+    if sumaD + D[i,j] + D[j,0] > maxD: break
+    
+    sumaD += D[i,j]
+    sumaP += ProfitC[j-1]
+    ProfitCT[j-1]=0
+    ClientsV.append(j)
+    i=j
+
+sumaD += D[i,0]
